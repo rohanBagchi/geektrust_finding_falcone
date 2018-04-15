e19746ccf8538e65269c27f3302aed967441648f
Rohan Bagchi <rohan.bagchi01@gmail.com>
2018-04-15 20:35:38 +0530

    Remove time_taken calculation from app container

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -9,6 +9,7 @@ function mapState(state) {
         errors: state.app_reducer.errors,
         has_error: state.app_reducer.has_error,
         is_loading: state.app_reducer.is_loading,
+        time_taken: state.app_reducer.time_taken,
         vehicles: state.vehicle_reducer.vehicles,
         planets: state.planet_reducer.planets,
         vehicles_form: state.vehicle_reducer.form,
@@ -37,7 +38,6 @@ function mergeProps(stateProps, dispatchProps, ownProps) {
         ...dispatchProps,
         ...ownProps,
         is_submit_btn_enabled: isSubmitButtonEnabled(planets_form, vehicles_form),
-        time_taken: getTimeTaken({ planets_form, vehicles_form, planets, vehicles }),
         findFalcone
     };
 }
@@ -72,24 +72,4 @@ function isAllVehiclesSelected(vehicles_form) {
         if (!selected_vehicle) return false;
     }
     return true;
-}
-
-export function getTimeTaken({ planets_form, vehicles_form, planets, vehicles }) {
-    const form_keys = Object.keys(planets_form);
-    const time_taken_list = [];
-
-    for (let i = 0; i < form_keys.length; i++) {
-        const selector = form_keys[i];
-        const { selected_planet } = planets_form[selector];
-        const { selected_vehicle } = vehicles_form[selector];
-
-        if (selected_planet && selected_vehicle) {
-            const selected_planet_entity = planets.filter(p => p.name === selected_planet)[0];
-            const selected_vehicle_entity = vehicles.filter(v => v.name === selected_vehicle)[0];
-            const time_taken = Math.floor(selected_planet_entity.distance / selected_vehicle_entity.speed);
-            time_taken_list.push(time_taken);
-        }
-    }
-
-    return time_taken_list.reduce((x, y) => x + y, 0);
 }
